c11fd187d5ebf48df4fe8f1735eed6013d2e69f0
Yiran Zhang <yiran.zhang@Yirans-MacBook-Pro.local>
2021-12-08 18:14:06 +0800

    修改了cypress->plugins->index.js：1、新增模块"cypress测试浏览器开发者工具自启动"

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -57,3 +57,28 @@ module.exports = (on, config) => {
     });
 
 };
+module.exports = (on, config) => {
+    on('before:browser:launch', (browser = {}, launchOptions) => {
+        // `args` is an array of all the arguments that will
+        // be passed to browsers when it launches
+        console.log(launchOptions.args) // print all current args
+
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+            // auto open devtools
+            launchOptions.args.push('--auto-open-devtools-for-tabs')
+        }
+
+        if (browser.family === 'firefox') {
+            // auto open devtools
+            launchOptions.args.push('-devtools')
+        }
+
+        if (browser.name === 'electron') {
+            // auto open devtools
+            launchOptions.preferences.devTools = true
+        }
+
+        // whatever you return here becomes the launchOptions
+        return launchOptions
+    })
+};

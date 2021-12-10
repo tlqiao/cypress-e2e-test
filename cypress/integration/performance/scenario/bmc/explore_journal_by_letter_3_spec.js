9a3f9ade6f9b9597d863e7c5fd432de821a4a13c
tlqiao <qiaotl@163.com>
2021-12-10 09:33:48 +0800

    add script

--- cypress/integration/performance/scenario/bmc/explore_journal_by_letter_3_spec.js
+++ cypress/integration/performance/scenario/bmc/explore_journal_by_letter_3_spec.js
@@ -1,8 +1,7 @@
 describe("scenario_003 for BMP test", () => {
     it("explore journal by letter", () => {
-        cy.visit("https://www.biomedcentral.com/journals");
-        cy.get('p a[href="/journals-a-z"]').click();
-        cy.get('p a[href="#Chemistry"]').click();
-        cy.get('li a[href="//biotechnologyforbiofuels.biomedcentral.com/"]').contains('Biotechnology for Biofuels').click();
+        cy.visit("https://www.biomedcentral.com/journals-a-z");
+        cy.get('a[href="#jump-to-C"]').contains('C').click();
+        cy.get('li a[href="//cancerandmetabolism.biomedcentral.com"]').contains('Cancer & Metabolism').click();
     })
 });
